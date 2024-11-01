c3d899f5814ee522956ebb76f49b9de5e882e3a3
hilal.aksoy <hilal.aksoy@std.antalya.edu.tr>
2024-11-01 16:38:16 +0300

    products table added

--- e-commerce-analysis.py
+++ e-commerce-analysis.py
@@ -3,7 +3,7 @@ import numpy as np
 
 np.random.seed(0)
 costumer_numbers=100
-
+product_numbers = 50
 customers= pd.DataFrame({
     'Customer ID': range(1, costumer_numbers + 1),
     'Customer Name': np.random.choice(['John', 'Sarah', 'Bob', 'Emily', 'Michael', 'David', 'Sophia', 'Olivia', 'William', 'James'], size=costumer_numbers),
@@ -13,5 +13,14 @@ customers= pd.DataFrame({
     'Purchase Category': np.random.choice(['Electronics', 'Clothing', 'Books'],costumer_numbers)
 
 })
+products = pd.DataFrame({
+    'Product ID': range(1, product_numbers + 1),
+    'Product Adı': np.random.choice(['Laptop', 'Phone', 'TV', 'Book', 'Shirt', 'Shoes'], product_numbers),
+    'Price': np.round(np.random.uniform(20, 5000, product_numbers), 2),  
+    'Category': np.random.choice(['Electronics', 'Clothing', 'Books'], product_numbers)
+})
+
+print("\nProducts table")
+print(products.head())
 print("Customer Table")
 print(customers.head())
